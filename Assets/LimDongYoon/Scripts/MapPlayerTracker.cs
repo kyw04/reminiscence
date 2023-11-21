8cdb6fed12605a644e10620b109e46b3cbdb5dcd
dkjk1234 <dkjk1234@naver.com>
2023-11-21 15:09:47 +0900

    SceneManager Add

--- Assets/LimDongYoon/Scripts/MapPlayerTracker.cs
+++ Assets/LimDongYoon/Scripts/MapPlayerTracker.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Map
 {
@@ -11,9 +12,13 @@ namespace Map
         public float enterNodeDelay = 1f;
         public MapManager mapManager;
         public MapView view;
+        public MySceneManager sceneManager;
 
         public static MapPlayerTracker Instance;
+        public enum BattleResult { None, Win, Lose }
+        public static BattleResult lastBattleResult = BattleResult.None;
 
+        
         public bool Locked { get; set; }
 
         private void Awake()
@@ -26,9 +31,12 @@ namespace Map
             if (Locked) return;
 
             // Debug.Log("Selected node: " + mapNode.Node.point);
-
+          
             if (mapManager.CurrentMap.path.Count == 0)
             {
+                
+                Debug.Log("Selected node: " + mapNode.Node.point);
+
                 // player has not selected the node yet, he can select any of the nodes with y = 0
                 if (mapNode.Node.point.y == 0)
                     SendPlayerToNode(mapNode);
@@ -37,6 +45,7 @@ namespace Map
             }
             else
             {
+                Debug.Log("Selected node: " + mapNode.Node.point);
                 var currentPoint = mapManager.CurrentMap.path[mapManager.CurrentMap.path.Count - 1];
                 var currentNode = mapManager.CurrentMap.GetNode(currentPoint);
 
@@ -58,11 +67,36 @@ namespace Map
 
             DOTween.Sequence().AppendInterval(enterNodeDelay).OnComplete(() => EnterNode(mapNode));
         }
+     
+        private void ReturnFromBattle()
+        {
+            if (lastBattleResult == BattleResult.Lose)
+            {
+                // 패배 로직 처리
+                // 예: 스테이지 재시도, 게임 오버 처리 등
+            }
+            else if (lastBattleResult == BattleResult.Win)
+            {
+                // 승리 로직 처리
+                // 예: 다음 스테이지로 진행
+            }
+        }
+
+        // 배틀 씬에서의 결과 설정
+        public static void SetBattleResult(BattleResult result)
+        {
+            lastBattleResult = result;
+        }
+
 
         private static void EnterNode(MapNode mapNode)
         {
-            // we have access to blueprint name here as well
+            // 배틀 씬으로 전환하기 전에 결과 초기화
+            lastBattleResult = BattleResult.None;
             Debug.Log("Entering node: " + mapNode.Node.blueprintName + " of type: " + mapNode.Node.nodeType);
+            // we have access to blueprint name here as well
+
+            MySceneManager.Instance.LoadNextScene();
             // load appropriate scene with context based on nodeType:
             // or show appropriate GUI over the map: 
             // if you choose to show GUI in some of these cases, do not forget to set "Locked" in MapPlayerTracker back to false
