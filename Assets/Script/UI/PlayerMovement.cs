9bf585205b42f2480a30123b8efe5ec25b759867
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2023-12-09 00:08:16 +0900

    feat: BattleScene Camera, Player Move

--- Assets/Script/UI/PlayerMovement.cs
+++ Assets/Script/UI/PlayerMovement.cs
@@ -6,9 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public Transform[] waypoints;
     public float moveSpeed = 5f;
+    public Transform playertransform;
+    public Transform targetPosition;
+    public GameObject Puzzlecanvas;
+    public GameObject canvas;
+    public bool canvasActivated = false;
 
     private int currentWaypoint = 0;
 
+    private void Start()
+    {
+        Puzzlecanvas.SetActive(false);
+        canvas.SetActive(false);
+    }
     void Update()
     {
         if (currentWaypoint < waypoints.Length)
@@ -24,6 +34,13 @@ public class PlayerMovement : MonoBehaviour
             if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f)
             {
                 currentWaypoint++;
+            }
+
+            if (!canvasActivated && Vector3.Distance(playertransform.position, targetPosition.position) < 0.5f)
+            {
+                Puzzlecanvas.SetActive(true);
+                canvas.SetActive(true);
+                canvasActivated = true;
             }
         }
     }
