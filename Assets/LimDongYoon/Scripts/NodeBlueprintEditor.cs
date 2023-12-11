e10371c55904823248004a2f1abf5b068d75a0c4
DakRo <dkjk1234@naver.com>
2023-12-11 09:51:57 +0900

    edit

--- Assets/LimDongYoon/Scripts/NodeBlueprintEditor.cs
+++ Assets/LimDongYoon/Scripts/NodeBlueprintEditor.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.Collections;
+using Unity.EditorCoroutines.Editor;
 
 namespace Map
 {
@@ -10,13 +12,14 @@ namespace Map
         SerializedProperty spriteProp;
         SerializedProperty spritesProp;
         SerializedProperty nodeElementalTypeProp;
+        bool showSprite = false;
 
         void OnEnable()
         {
             nodeTypeProp = serializedObject.FindProperty("nodeType");
             spriteProp = serializedObject.FindProperty("sprite");
             spritesProp = serializedObject.FindProperty("sprites");
-            nodeElementalTypeProp = serializedObject.FindProperty("nodeElementalTypeProp");
+            nodeElementalTypeProp = serializedObject.FindProperty("nodeElementalType");
         }
 
         public override void OnInspectorGUI()
@@ -25,20 +28,53 @@ namespace Map
 
             EditorGUILayout.PropertyField(nodeTypeProp);
             
-
             // MinorEnemy일 때만 sprites 배열을 표시
-            if ((NodeType)nodeTypeProp.enumValueIndex == NodeType.MinorEnemy)
+            if ((NodeType)nodeTypeProp.enumValueIndex == NodeType.MinorEnemy || (NodeType)nodeTypeProp.enumValueIndex == NodeType.Boss )
             {
                 EditorGUILayout.PropertyField(spritesProp, true);
                 EditorGUILayout.PropertyField(nodeElementalTypeProp);
-                
+                EditorGUILayout.PropertyField(spriteProp);
             }
             else
             {
                 EditorGUILayout.PropertyField(spriteProp); // 항상 표시될 sprite 필드
             }
 
+            if (GUILayout.Button("Show Sprite After Delay"))
+            {
+                EditorCoroutineUtility.StartCoroutineOwnerless(ShowSpriteAfterDelay(5)); // 5초 지연
+            }
+
+            if (showSprite)
+            {
+                DrawSpritePreview(spriteProp);
+            }
+
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private IEnumerator ShowSpriteAfterDelay(float delay)
+        {
+            showSprite = false;
+            yield return new WaitForSeconds(delay);
+            showSprite = true;
+        }
+
+        private void DrawSpritePreview(SerializedProperty spriteProp)
+        {
+            Sprite sprite = (Sprite)spriteProp.objectReferenceValue;
+            if (sprite != null)
+            {
+                Rect spriteRect = new Rect(sprite.textureRect.x / sprite.texture.width,
+                    sprite.textureRect.y / sprite.texture.height,
+                    sprite.textureRect.width / sprite.texture.width,
+                    sprite.textureRect.height / sprite.texture.height);
+
+                GUILayout.Label("", GUILayout.Height(400)); // 또는 원하는 높이
+                Rect lastRect = GUILayoutUtility.GetLastRect();
+                GUI.DrawTextureWithTexCoords(lastRect, sprite.texture, spriteRect);
+                // DrawSpritePreview 함수 구현은 이전과 동일
+            }
         }
     }
 }
