b9b298d433f33adabcddcca3977e18aa7fd0c485
tigerspower <pmj0316985@gmail.com>
2023-12-11 15:18:59 +0900

    Augment

--- Assets/LimDongYoon/Aguments/Augment.cs
+++ Assets/LimDongYoon/Aguments/Augment.cs
@@ -1,4 +1,5 @@
 
+using Map;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewAugment", menuName = "Augment/Augment")]
@@ -51,6 +52,8 @@ public class Augment : ScriptableObject
     public ActionType actionType;
     public int priority;
     public AugmentType augmentType;
+    public NodeBase nodeBase;
+    public PlayerMovement playerMovement;
 
 
     public Augment(int id, string name, string description, ActionType actionType)
@@ -68,11 +71,48 @@ public class Augment : ScriptableObject
         {
             case 40000:
                 // "쌍둥이의 검" 효과 구현
+                TwinBlades();
+                
                 break;
             // 다른 증강체 효과들을 여기에 구현합니다.
+
+            case 40001:
+
+            case 40002:
+                Eclipse();
+                break;
             // ...
             default:
                 break;
+        }
+    }
+
+    public void TwinBlades()
+    {
+        if (GameManager.instance.foundPatternCount == 2 && GameManager.instance.gameState == GameState.EndTurn)
+        {
+            GameManager.instance.enemy.GetDamage(nodeBase, 10);
+        }
+    }
+
+    public void Fatereject()
+    {
+
+    }
+
+    public void Eclipse()
+    {
+        if(playerMovement.canvasActivated)
+        {
+           
+        }
+    }
+
+    public void TeacherHat()
+    {
+        if(GameManager.instance.gameState == GameState.End)
+        {
+ 
         }
     }
 }
