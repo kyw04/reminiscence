c149c24cc7e2bbc22ba5b52e498d18c641593cfd
Fsahn <haider1315s@gmail.com>
2023-12-12 01:34:59 +0900

    ms

--- Assets/Scriptable Object/Script/EnemyMaterial.cs
+++ Assets/Scriptable Object/Script/EnemyMaterial.cs
@@ -16,11 +16,16 @@ public struct Meterials
 public class EnemyMaterial : MonoBehaviour
 {
     public Animator animator;
+    public MeshRenderer defaultMeshRenderer;
+    public Material[] defaultRendererMaterials;
     public SkinnedMeshRenderer[] meshRenderer;
     public Meterials[] meterial;
 
     public void SetMaterials(int index)
     {
+        if (defaultMeshRenderer)
+            defaultMeshRenderer.material = defaultRendererMaterials[index];
+
         for (int i = 0; i < meshRenderer.Length; i++)
         {
             meshRenderer[i].materials = meterial[index].index[i].materials;
